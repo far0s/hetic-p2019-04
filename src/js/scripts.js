56e486352b9b200e685d32d4fa10608c8c0b824e
Sebastien Dancer-Michel <s.dancermichel@gmail.com>
2016-11-14 00:26:23 +0100

    [HOME][TRANSITIONS][VARIOUS] Home responsive, navigation :target system, warning message in the console
    The warning message in the console is directed at children

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,12 @@
 (function ($, window, document, undefined) {
   'use strict';
   $(function () {
+    // Log an informal message saying this is a "developer zone"
+    console.warn('Bonjour ! Ceci est la console développeur, il vaut mieux que tu ne touches à rien ici si tu ne veux pas casser le jeu :-)');
+    console.warn('Pour fermer la console, appuie sur la petite croix en haut à droite.');
 
-    console.log('rosetta');
+    // At start, go straight to #home page
+    location.href='#home';
 
   });
 })(jQuery, window, document);
